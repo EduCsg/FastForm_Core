ba1bca4df19eead360e4194f287e7be98ec59bc4
Eduardo <eduardocasagrande71@gmail.com>
2025-02-16 18:01:28 -0300

    First commit

---
+++ src/main/java/com/fastForm/core/FastFormCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.fastForm.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFormCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFormCoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastForm/core/FastFormCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastForm.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFormCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
